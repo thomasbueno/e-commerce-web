cb0102022bf39b233a04a6f649f2b95d837d0a37
Thomás Escolástico Bueno <62290004+thomasbuenoU@users.noreply.github.com>
2021-02-15 20:03:05 -0300

    creating StoreFront Footer

--- components/shared/MainComponent/index.tsx
+++ components/shared/MainComponent/index.tsx
@@ -1,14 +1,17 @@
 import React from "react"
 import Header from "./Header/StoreFrontHeader"
+import Footer from "./Footer/StoreFrontFooter"
 
 const MainComponent: React.FC = ({ children }) => {
   return (
     <div className="d-flex flex-column sticky-footer-wrapper">
       <Header />
 
-      <div className="container fleex-fill">
+      <div className="container flex-fill">
         {children}
       </div>
+
+      <Footer />
     </div>
   )
 }
