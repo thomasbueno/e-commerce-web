6ab38228c5e3cb986386a3166d23418429dc5895
Thomás Escolástico Bueno <62290004+thomasbuenoU@users.noreply.github.com>
2021-02-27 23:10:15 -0300

    configuring redux to application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { AppProps } from "next/app";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Head from 'next/head'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "../store";
 
 import "../styles/globals.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <title>DevGames</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Head>
+            <title>DevGames</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          </Head>
 
-      <Component {...pageProps} />
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
     </>
   )
 }
